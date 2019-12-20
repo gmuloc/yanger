33eb8ec6e2ed2cec06f36f387c43973b5fccfc7c
gmuloche <gmuloche@cisco.com>
2019-12-20 11:55:41 +0100

    Fix env for libxml after Catalina

--- env.sh
+++ env.sh
@@ -7,3 +7,10 @@ p=`pwd`
 export PATH=$p/bin:$PATH
 export YANG_MODPATH=$p/modules:$YANG_MODPATH
 export W=$p
+
+if [[ "$(uname 2> /dev/null)" == "Darwin" ]]; then
+  echo "Using Mac OS version $(sw_vers -productVersion)"
+  if [[ "$(sw_vers -productVersion)" == "10.15"* ]]; then
+    export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
+  fi
+fi
